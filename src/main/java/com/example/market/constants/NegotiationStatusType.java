f8817e5b95f3c8912e0d2dd3bdfdc827e1f20d93
harper <jhjin2u@gmail.com>
2023-07-05 22:18:10 +0900

    [#11] feat : Negotiation Entity 추가

---
+++ src/main/java/com/example/market/constants/NegotiationStatusType.java
@@ -0,0 +1,11 @@
+package com.example.market.constants;
+
+import lombok.Getter;
+
+@Getter
+public enum NegotiationStatusType {
+	제안,
+	수락,
+	확정,
+	거절
+}
--- src/main/java/com/example/market/dto/response/ResponseMessage.java
+++ src/main/java/com/example/market/dto/response/ResponseMessage.java
@@ -10,11 +10,17 @@ public enum ResponseMessage {
 	SUCCESS_ITEM_MODIFY("물품이 수정되었습니다."),
 	SUCCESS_ITEM_DELETE("물품을 삭제했습니다."),
 	SUCCESS_ITEM_IMAGE("이미지가 등록되었습니다."),
+
 	SUCCESS_COMMENT_CREATE("댓글이 등록되었습니다."),
 	SUCCESS_COMMENT_MODIFY("댓글이 수정되었습니다."),
 	SUCCESS_COMMENT_REPLY_CREATE("댓글에 답변이 추가되었습니다."),
-	SUCCESS_COMMENT_DELETE("댓글을 삭제했습니다.")
+	SUCCESS_COMMENT_DELETE("댓글을 삭제했습니다."),
 
+	SUCCESS_NEGOTIATION_CREATE("구매 제안이 등록되었습니다."),
+	SUCCESS_NEGOTIATION_MODIFY("제안이 수정되었습니다."),
+	SUCCESS_NEGOTIATION_DELETE("제안을 삭제했습니다."),
+	SUCCESS_NEGOTIATION_STATUS("제안의 상태가 변경되었습니다."),
+	SUCCESS_NEGOTIATION_APPROVED("구매가 확정되었습니다.")
 	;
 
 	private final String message;
